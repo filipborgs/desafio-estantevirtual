08343f12ec4cb5ffc68e28a2ef88bfe12d3249b3
Filipe <filipborgs48@gmail.com>
2021-05-18 21:26:20 -0300

    Address form and CEP autofill

--- src/components/endereco/EnderecoForm.vue
+++ src/components/endereco/EnderecoForm.vue
@@ -1,9 +1,129 @@
 <template>
-  <div></div>
+  <b-row>
+    <b-col cols="6">
+      <b-form-group
+        label="CEP: "
+        label-for="cep-input"
+        description="Digite o seu CEP para buscar o endereço"
+      >
+        <b-form-input
+          id="cep-input"
+          v-model="address.cep"
+          placeholder="Digite o CEP"
+          v-mask="'#####-###'"
+          @input="buscarCep"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </b-col>
+
+    <b-col cols="6">
+      <b-form-group label="Estado: " label-for="uf-input">
+        <b-form-select
+          class="form-control"
+          id="uf-input"
+          :disabled="!cepValid"
+          v-model="address.uf"
+          :options="ufs"
+          value-field="sigla"
+          text-field="nome"
+        ></b-form-select>
+      </b-form-group>
+    </b-col>
+
+    <b-col cols="12">
+      <b-form-group label="Logradouro:" label-for="logradouro-input">
+        <b-form-input
+          id="logradouro-input"
+          :disabled="!cepValid"
+          v-model="address.logradouro"
+          placeholder="Digite o Logradouro"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </b-col>
+
+    <b-col cols="6">
+      <b-form-group label="Bairro:" label-for="bairro-input">
+        <b-form-input
+          id="bairro-input"
+          :disabled="!cepValid"
+          v-model="address.bairro"
+          placeholder="Digite o Bairro"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </b-col>
+
+    <b-col cols="6">
+      <b-form-group label="Cidade:" label-for="cidade-input">
+        <b-form-input
+          id="cidade-input"
+          :disabled="!cepValid"
+          v-model="address.localidade"
+          placeholder="Digite o Cidade"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </b-col>
+
+    <b-col cols="12">
+      <b-form-group label="Complemento:" label-for="complemento-input">
+        <b-form-input
+          id="complemento-input"
+          :disabled="!cepValid"
+          v-model="address.complemento"
+          placeholder="Digite o Complemento"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
-export default {};
+export default {
+  async mounted() {
+    const { data } = await this.$axios.get(
+      "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+    );
+    this.ufs = [{ sigla: null, nome: "Selecione uma UF" }, ...data];
+  },
+  data() {
+    return {
+      address: {
+        uf: "BA",
+        logradouro: null,
+        cep: null,
+        bairro: null,
+        localidade: null,
+        complemento: null,
+      },
+      ufs: [],
+    };
+  },
+
+  methods: {
+    async buscarCep(cep) {
+      if (!cep || cep.length != 9) return;
+
+      const { data } = await this.$axios.get(
+        `http://viacep.com.br/ws/${cep}/json`
+      );
+
+      for (const key in this.address) {
+        this.address[key] = data[key];
+      }
+      console.log(this.address);
+    },
+  },
+
+  computed: {
+    cepValid() {
+      return this.address.cep?.length == 9;
+    },
+  },
+};
 </script>
 
 <style></style>
